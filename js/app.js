bd73ee143f31a50d2e1b685dc838bd9121d19fd3
escuderopc <escuderop.carina@gmail.com>
2022-05-18 21:11:16 -0500

    Vista del login version 1.1

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//Variables
+const inputUser = document.querySelector('#inputUser')
+const inputPassword = document.querySelector('#inputPassword')
+const formLogin = document.querySelector('#formLogin')
+
+document.addEventListener('DOMContentLoaded', () =>{
+    mostrarHTML(cuentas)
+})
---
+++ js/db.js
@@ -0,0 +1,20 @@
+//data base de los clientes del banco bitcoin Neo
+//Se esta usando un array con 3 clientes como ejemplo
+
+var cuentas = [
+    {
+        user: "Carina",
+        pass: 12345678,
+        saldo: 990
+    },
+    {
+        user: "Kim Shin",
+        pass: 2335555,
+        saldo: 500
+    },
+    {
+        user: "Gong Yoo",
+        pass: 987654321,
+        saldo: 990
+    }
+]
