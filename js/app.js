4160c5b69cef92f78f0bf17f596467711b70456e
escuderopc <escuderop.carina@gmail.com>
2022-05-19 14:37:33 -0500

    Cajero automático version 1.0

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
 //Variables
-const inputUser = document.querySelector('#inputUser')
-const inputPassword = document.querySelector('#inputPassword')
-const formLogin = document.querySelector('#formLogin')
+//const inputUser = document.querySelector('#inputUser')
+//const inputPassword = document.querySelector('#inputPassword')
+//const formLogin = document.querySelector('#formLogin')
 
-document.addEventListener('DOMContentLoaded', () =>{
-    mostrarHTML(cuentas)
-})
+//Trae el elemento 
+//let inputUser = document.getElementById("inputUser")
+//let inputPassword  = document.getElementById("inputPassword")
+let formLogin  = document.getElementById("formLogin")
+
+//console.log("user", inputUser)
+
+function login(form){
+
+    if(form.inputUser.value=="carina"){
+        if(form.inputPassword.value=="1234"){
+            location = "banconeo.html"
+        }else{
+            alert("La contraseña es incorrecta")
+        } 
+    }else{
+        alert("El usuario no existe")
+    } 
+
+}
